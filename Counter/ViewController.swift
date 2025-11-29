2d445cbf3ebd1a48d6caff0ded26d82b2351e514
borisryavkin <borisry@icloud.com>
2025-11-30 09:27:29 +1000

    added visualisation for the counter, ready for submition of base project

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,14 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     var counter: Int = 0
-
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "0"
     }
     @IBAction func increaseUIButton(_ sender: Any) {
         counter+=1
-        print(counter)
+        counterLabel.text = "\(counter)"
     }
     
 
