444af79565f75542065e4a1581af2cbbac9b2ff2
borisryavkin <borisry@icloud.com>
2025-11-29 15:48:46 +1000

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Boris Ryavkin on 29/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
